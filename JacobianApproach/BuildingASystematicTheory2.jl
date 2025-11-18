529135432dee338da65cc9e2a7fdf98efea94212
Nico Merino <nicolasmerino41@gmail.com>
2025-11-18 17:28:05 +0100

    Update BuildingASystematicTheory2.jl

--- JacobianApproach/BuildingASystematicTheory2.jl
+++ JacobianApproach/BuildingASystematicTheory2.jl
@@ -131,8 +131,8 @@ function plot_three_tests(res)
 
         # |ΔR̃med|
         ax2 = Axis(fig[2, j]; xscale=log10, ylabel=(j==1 ? "|ΔR̃med|" : ""), xlabel="t")
-        values = abs.(f0 .- others[j])
-        values[values .< 1e-3] .= 0.0
+        values = f0 .- others[j]
+        # values[values .< 1e-3] .= 0.0
         lines!(ax2, t_vals, values; color=:crimson, linewidth=2)
 
         # eigenvalue real parts
